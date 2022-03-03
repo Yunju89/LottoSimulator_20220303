57746573083e33ea24a9218a18be930780613400
Yunju89 <toyaann@naver.com>
2022-03-03 10:32:10 +0900

    Initial commit

---
+++ app/src/main/java/org/techtown/lottosimulator_20220303/MainActivity.kt
@@ -0,0 +1,11 @@
+package org.techtown.lottosimulator_20220303
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
