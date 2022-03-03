51df93715f85d1b86960cb6e6c69ad07bb2a33c8
Yunju89 <toyaann@naver.com>
2022-03-03 15:48:32 +0900

    자동 반복 구매- while 등의 일반 반복문으로는 UI 적용 어렵다.

--- app/src/main/java/org/techtown/lottosimulator_20220303/MainActivity.kt
+++ app/src/main/java/org/techtown/lottosimulator_20220303/MainActivity.kt
@@ -52,6 +52,20 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+        btnAutoBuyLotto.setOnClickListener {
+//            처음 눌리면 > 반복구매 시작 > 1천만원 사용할 때 까지
+
+//            단순 반복 > 반복 속도가 너무 빨라서, UI가 멈춘 것 처럼 보인다.
+            while(true){
+                buyLotto()
+
+                if(mUsedMoney >= 10000000)
+                    break
+            }
+
+//            반복 구매 중에 눌리면 > 반복 종료
+        }
+
     }
 
     private fun buyLotto(){
