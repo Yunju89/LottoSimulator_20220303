577d478c34c3c4f9985d53c31d7f141e3c6bb5ad
Yunju89 <toyaann@naver.com>
2022-03-03 12:24:18 +0900

    당첨 번호 목록 > 텍스트뷰 목록에 반영  - 구글링 : forEachIndexed 기능 활용

--- app/src/main/java/org/techtown/lottosimulator_20220303/MainActivity.kt
+++ app/src/main/java/org/techtown/lottosimulator_20220303/MainActivity.kt
@@ -3,12 +3,16 @@ package org.techtown.lottosimulator_20220303
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
 //    컴퓨터가 뽑은 당첨번호 6개를 저장할 ArrayList
     val mWinNumberList = ArrayList<Int>()
+
+//    당첨번호를 보여줄 6개의 텍스트뷰를 담아 둘 ArrayList
+    val mWinNumTextViewList = ArrayList<TextView>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -63,7 +67,12 @@ class MainActivity : AppCompatActivity() {
 
         Log.d("당첨번호", mWinNumberList.toString())
 
-        for(winNum in mWinNumberList){
+//        for > 돌면서, 당첨번호도 / 몇번째 바퀴인지도 필요 => 텍스트뷰를 찾아내야 함.
+
+        mWinNumberList.forEachIndexed { index, winNum ->
+//            순서에 맞는 텍스트뷰 추출 => 문구로 당첨번호 설정}
+
+            mWinNumTextViewList[index].text = winNum.toString()
 
         }
 
@@ -73,6 +82,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setValues(){
+
+        mWinNumTextViewList.add(txtWinNum01)
+        mWinNumTextViewList.add(txtWinNum02)
+        mWinNumTextViewList.add(txtWinNum03)
+        mWinNumTextViewList.add(txtWinNum04)
+        mWinNumTextViewList.add(txtWinNum05)
+        mWinNumTextViewList.add(txtWinNum06)
 
     }
 }
